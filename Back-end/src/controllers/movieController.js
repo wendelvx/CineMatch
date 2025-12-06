a7f866141c3189274b8f4cbf02b71f23e1258536
wendel <capauzumaki@gmail.com>
2025-12-06 20:48:13 -0300

    Adicionando seed e aumentando quantidade de filmes por sala

--- Back-end/src/controllers/movieController.js
+++ Back-end/src/controllers/movieController.js
@@ -2,22 +2,62 @@ const tmdbService = require('../services/tmdbService');
 const { PrismaClient } = require('@prisma/client');
 const prisma = new PrismaClient();
 
+
+const getSeedFromCode = (code) => {
+  if (!code) return 1;
+  let hash = 0;
+  for (let i = 0; i < code.length; i++) {
+    const char = code.charCodeAt(i);
+    hash = ((hash << 5) - hash) + char;
+    hash = hash & hash; 
+  }
+  return Math.abs(hash); 
+};
+
+
+const shuffleWithSeed = (array, seed) => {
+  let m = array.length, t, i;
+  
+  const random = () => {
+    var x = Math.sin(seed++) * 10000;
+    return x - Math.floor(x);
+  };
+
+  while (m) {
+    i = Math.floor(random() * m--);
+    t = array[m];
+    array[m] = array[i];
+    array[i] = t;
+  }
+  return array;
+};
+
 exports.listMovies = async (req, res) => {
   try {
-    const { roomCode, page } = req.query;
+    const { roomCode } = req.query; 
 
     let genreToSearch = '28'; 
+    let seed = 1;
 
     if (roomCode) {
+      seed = getSeedFromCode(roomCode);
+
       const room = await prisma.room.findUnique({ where: { code: roomCode } });
       if (room) {
         genreToSearch = room.genreId;
       }
     }
 
-    const movies = await tmdbService.getMovies(genreToSearch, page);
+   
+    const startPage = (seed % 10) + 1; 
+
+    let movies = await tmdbService.getMovies(genreToSearch, startPage);
     
+    
+    movies = shuffleWithSeed(movies, seed);
+
     return res.json(movies);
+
   } catch (error) {
     console.error(error);
     return res.status(500).json({ error: 'Erro ao buscar filmes.' });
--- Back-end/src/services/tmdbService.js
+++ Back-end/src/services/tmdbService.js
@@ -3,22 +3,29 @@ const axios = require('axios');
 const TMDB_BASE_URL = 'https://api.themoviedb.org/3';
 const API_KEY = process.env.TMDB_API_KEY;
 
-exports.getMovies = async (genreId, page = 1) => {
+exports.getMovies = async (genreId, startPage = 1) => {
   try {
-    const response = await axios.get(`${TMDB_BASE_URL}/discover/movie`, {
-      params: {
-        api_key: API_KEY,
-        language: 'pt-BR',
-        sort_by: 'popularity.desc', 
-        include_adult: false,
-        include_video: false,
-        page: page,
-        with_genres: genreId 
-      }
-    });
+    const pagesToFetch = [startPage, startPage + 1, startPage + 2];
 
-   
-    const formattedMovies = response.data.results.map(movie => ({
+    const requests = pagesToFetch.map(page => 
+      axios.get(`${TMDB_BASE_URL}/discover/movie`, {
+        params: {
+          api_key: API_KEY,
+          language: 'pt-BR',
+          sort_by: 'popularity.desc',
+          include_adult: false,
+          include_video: false,
+          page: page,
+          with_genres: genreId
+        }
+      })
+    );
+
+    const responses = await Promise.all(requests);
+
+    const allMovies = responses.flatMap(response => response.data.results);
+
+    const formattedMovies = allMovies.map(movie => ({
       id: movie.id,
       title: movie.title,
       overview: movie.overview,
@@ -27,7 +34,13 @@ exports.getMovies = async (genreId, page = 1) => {
       vote_average: movie.vote_average
     }));
 
-    return formattedMovies;
+    const uniqueMovies = formattedMovies.filter((movie, index, self) =>
+      index === self.findIndex((t) => (
+        t.id === movie.id
+      ))
+    );
+
+    return uniqueMovies;
 
   } catch (error) {
     console.error('Erro na TMDB:', error.message);
